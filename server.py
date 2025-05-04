9ceb5dcf526fb4092d28a8a0bca118aededbb446
cry8267 <1781356168@qq.com>
2025-05-05 02:29:40 +0800

    The core logic of server-side processing of client requests
    This new function is responsible for parsing the command strings sent by the client, performing operations on the tuple space according to the command type (R read, G get and delete, P insert), and then generating the corresponding response message return. If the format is wrong or the operation fails, an error message is returned.

--- server.py
+++ server.py
@@ -28,4 +28,56 @@ def handle_client(conn,addr):
     finally:
         conn.close()  #Close the connection, no matter what
 
+
+# Handle messages from the client
+def deal(message):
+    parts=message.split(' ',2)
+    if len(parts)<2:
+        return "020 ERR bad format"
+    cmd=parts[1]
+    if cmd =='R':  #read operation
+        ok,v=ts.read(parts[2])
+        if ok:
+            stats['read']+=1
+            stats['ops']+=1
+            res=f"OK ({parts[2]}, {v}) read"
+            return f"{len(res)+4:03} {res}"
+        else:
+            stats['error']+=1
+            stats['ops']+=1
+            return f"024 ERR {parts[2]} does not exist"
+    elif cmd =='G':  #get operation
+        ok,v=ts.get(parts[2])
+        if ok:
+            stats['get']+=1
+            stats['ops']+=1
+            res=f"OK ({parts[2]}, {v}) removed"
+            return f"{len(res)+4:03} {res}"
+        else:
+            stats['error']+=1
+            stats['ops']+=1
+            return f"024 ERR {parts[2]} does not exist"
+    elif cmd =='P':  #put operation
+        if len(parts)<3:
+            return "020 ERR bad format"
+        k_v=parts[2].split(' ',1)
+        if len(k_v) !=2:
+            return "020 ERR bad format"
+        k,v=k_v
+        ok=ts.put(k,v)  #put tuples
+        if ok:
+            stats['put']+=1
+            stats['ops']+=1
+            res = f"OK ({k}, {v}) added"
+            return f"{len(res)+4:03} {res}"
+        else:
+            stats['error']+=1
+            stats['ops']+=1
+            return f"024 ERR {k} already exists"
+    else:  
+        stats['error']+=1  #Handle unknown information
+        return"020 ERR unknown cmd"
+    
+            
+
     
